e349b3af44aded32ee241c83606e0bc2dc11b9ee
Ing. Eduardo E. Enríquez <rekineke@hotmail.com>
2024-06-11 22:12:50 -0500

    Agragando validaciones al formulario

--- js/app.js
+++ js/app.js
@@ -3,19 +3,60 @@ document.addEventListener('DOMContentLoaded', function() {
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
+  const formulario = document.querySelector("#formulario");
 
   //console.log(inputMensaje)
 
   //Asignar eventos de la interfaz
-  inputEmail.addEventListener('blur', validar);
+  inputEmail.addEventListener("blur", validar);
 
-  inputAsunto.addEventListener('blur', validar);
+  inputAsunto.addEventListener("blur", validar);
 
-  inputMensaje.addEventListener('blur', validar);
+  inputMensaje.addEventListener("blur", validar);
 
-  function validar(evento) {
-    console.log(evento.target.value);
+  function validar(e) {
+    //console.log(e.target.parentElement);
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
+    }
+
+    if (e.target.id === "email" && !validarEmail(e.target.value)) {
+      mostrarAlerta(`El email no es valido`, e.target.parentElement);
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
   }
 
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
+    //Generar alerta en HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+    //Insertar alerta en HTML
+    referencia.appendChild(error);
+  }
+
+  function limpiarAlerta(referencia) {
+    //Comprueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove(); // Elimina la alerta anterior si existe
+    }
+  }
+
+  function validarEmail(email) {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const resultado = regex.test(String(email).toLowerCase());
+    console.log(resultado);
+    return resultado;
+  }  
 });
 
